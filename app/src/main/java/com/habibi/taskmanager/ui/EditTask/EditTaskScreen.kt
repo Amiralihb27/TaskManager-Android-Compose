6fe55b12ecb1ca77c6dd5cc05b987f4c133f44b1
Amirali <amiralihabibi82@gmail.com>
2025-09-24 11:14:11 +0330

    feat(editTask): Add screen for editing task details
    This commit introduces the `EditTaskScreen` composable, enabling users to modify task details.
    
    Key features:
    - **Title and Description Editing**: Users can update the task title and description using `TextField` components.
    - **Category Selection**: A `LazyRow` displays category chips, allowing users to change the task's category.
    - **Due Date Modification**: Integrates `DatePickerDialog` and `TimePickerDialog` for selecting a new due date and time.
    - **Automatic Saving**: Changes are saved automatically when modifications are made to title, description, category, or due date.
    - **Top App Bar**: Includes a back button for navigation and removes the explicit "Save" button due to automatic saving.
    - **UI State Handling**: Manages loading and error states.
    - **ViewModel Integration**: Relies on `EditTaskViewModel` for loading task data and handling update logic.
    
    The screen structure includes:
    - `EditTaskScreen`: Main composable that handles UI state and navigation.
    - `EditTaskForm`: Contains the input fields and logic for editing task attributes.
    - `DatePickerModal` and `TimePickerDialog`: Helper composables for date and time selection.
    - `CategoryChip` and `FilterCategoryRow`: Composables for displaying and selecting categories.

---
+++ app/src/main/java/com/habibi/taskmanager/ui/EditTask/EditTaskScreen.kt
@@ -0,0 +1,395 @@
+package com.habibi.taskmanager.ui.EditTask
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Description
+import androidx.compose.material.icons.filled.Schedule
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.text.format.DateFormat
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.platform.LocalContext
+import com.habibi.taskmanager.ui.EditTask.EditTaskViewModel
+import com.habibi.taskmanager.ui.categories.CategoryDetails
+import com.habibi.taskmanager.ui.task.TasksDetails
+import java.util.Calendar
+import java.util.TimeZone
+import java.util.Date
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditTaskScreen(
+    navigateBack: () -> Unit,
+    viewModel: EditTaskViewModel,
+    taskId: Int
+) {
+    // Load the task when the screen is first shown
+    LaunchedEffect(key1 = taskId) {
+        viewModel.loadTask(taskId)
+    }
+
+    val uiState by viewModel.uiState.collectAsState()
+
+//      un comment the section below if you are using save button
+//    LaunchedEffect(key1 = uiState.isTaskSaved) {
+//        if (uiState.isTaskSaved) {
+//            navigateBack()
+//        }
+//    }
+
+    Scaffold(
+        topBar = {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp),
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = navigateBack) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+
+                    //textbutton for save changes
+
+//                    TextButton(onClick = { viewModel.saveTask() }) {
+//                        Text(
+//                            text = "Save",
+//                            color = MaterialTheme.colorScheme.onSurface
+//                        )
+//                    }
+                }
+            }
+        }
+    ) { paddingValues ->
+        when {
+            uiState.isLoading -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            uiState.taskDetails != null -> {
+                val taskDetails = uiState.taskDetails!!
+                EditTaskForm(
+                    taskDetails = taskDetails,
+                    allCategories = uiState.categoryList,
+                    onCategoryChange = viewModel::onCategoryChange,
+                    onTitleChange = viewModel::onTitleChange,
+                    onDueDateChange = viewModel::onDueDateChange,
+                    onSave = viewModel::saveTask,
+                    onDescriptionChange = viewModel::onDescriptionChange,
+                    modifier = Modifier.padding(paddingValues)
+                )
+            }
+
+            else -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("Task not found.")
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditTaskForm(
+    taskDetails: TasksDetails,
+    allCategories: List<CategoryDetails>,
+    onCategoryChange: (Int) -> Unit,
+    onTitleChange: (String) -> Unit,
+    onDueDateChange: (Long?) -> Unit, // This is your existing VM function
+    onSave: () -> Unit, // This is your existing VM function
+    onDescriptionChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current // Needed for the classic dialog
+    var showDatePicker by remember { mutableStateOf(false) }
+    var showTimePicker by remember { mutableStateOf(false) }
+    var selectedDateMillis by remember { mutableStateOf<Long?>(null) }
+
+    // Use the safe, backward-compatible Android DateFormat class for display
+    val formattedDateString = taskDetails.dueDate?.let { millis ->
+        DateFormat.format("d MMMM yyyy HH:mm", Date(millis)).toString()
+    } ?: "Set due date"
+
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        // --- Task Title ---
+        TextField(
+            value = taskDetails.title,
+            onValueChange = { newValue ->
+                onTitleChange(newValue)
+                onSave()
+            },
+            modifier = Modifier.fillMaxWidth(),
+            textStyle = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.SemiBold),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // --- Category Section ---
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Bookmark,
+                contentDescription = "Bookmark",
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .size(24.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            FilterCategoryRow(
+                allCategories = allCategories,
+                selectedCategoryId = taskDetails.categoryId,
+                onCategoryClick = onCategoryChange,
+                onSave = onSave
+            )
+        }
+
+        // --- Task Description ---
+        TextField(
+            value = taskDetails.description,
+            onValueChange = { newValue ->
+                onDescriptionChange(newValue)
+                onSave()
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Description,
+                    contentDescription = "Description",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+            )
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Divider()
+
+        // --- Alarm Section ---
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+                .clickable { showDatePicker = true }
+        ) {
+            Icon(Icons.Default.Schedule, contentDescription = "Due Date")
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = formattedDateString,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+        Divider()
+    }
+
+    if (showDatePicker) {
+        DatePickerModal(
+            initialSelectedDateMillis = taskDetails.dueDate,
+            onDateSelected = { dateInMillis ->
+                selectedDateMillis = dateInMillis
+                showTimePicker = true // Triggers the time picker
+            },
+            onDismiss = {
+                showDatePicker = false
+            }
+        )
+    }
+
+
+    if (showTimePicker && selectedDateMillis != null) {
+        val calendar = Calendar.getInstance()
+        taskDetails.dueDate?.let {
+            calendar.timeInMillis = it
+        }
+        val timePickerState = rememberTimePickerState(
+            initialHour = calendar.get(Calendar.HOUR_OF_DAY),
+            initialMinute = calendar.get(Calendar.MINUTE),
+            is24Hour = true,
+        )
+
+        TimePickerDialog(
+            onDismiss = { showTimePicker = false },
+            onConfirm = {
+                val finalCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC")).apply {
+                    timeInMillis = selectedDateMillis!!
+                    set(Calendar.HOUR_OF_DAY, timePickerState.hour)
+                    set(Calendar.MINUTE, timePickerState.minute)
+                    set(Calendar.SECOND, 0)
+                    set(Calendar.MILLISECOND, 0)
+                }
+                onDueDateChange(finalCalendar.timeInMillis)
+                onSave()
+                showTimePicker = false
+            },
+            content = {
+                TimePicker(state = timePickerState)
+            }
+
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit,
+    initialSelectedDateMillis: Long?
+) {
+
+    val datePickerState =
+        rememberDatePickerState(initialSelectedDateMillis = initialSelectedDateMillis)
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+
+@Composable
+fun TimePickerDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) {
+                Text("Dismiss")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onConfirm() }) {
+                Text("OK")
+            }
+        },
+        text = { content() }
+    )
+}
+
+object CategoryChipColors {
+    val chipSelectedText = Color.Black
+    val chipUnselectedText = Color.White
+    val chipUnselectedBg = Color.Transparent
+    val chipUnselectedBorder = Color(0xFF757575) // Grey
+}
+
+@Composable
+fun CategoryChip(
+    text: String,
+    backgroundColor: Color,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val textColor =
+        if (isSelected) CategoryChipColors.chipSelectedText else CategoryChipColors.chipUnselectedText
+    val border =
+        if (isSelected) null else BorderStroke(1.dp, CategoryChipColors.chipUnselectedBorder)
+
+    Surface(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        color = backgroundColor,
+        border = border
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun FilterCategoryRow(
+    allCategories: List<CategoryDetails>,
+    selectedCategoryId: Int?,
+    onCategoryClick: (Int) -> Unit,
+    onSave: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(allCategories) { category ->
+            val isThisChipSelected = (category.categoryId == selectedCategoryId)
+            val chipBackgroundColor = if (isThisChipSelected) {
+                Color(android.graphics.Color.parseColor(category.color))
+            } else {
+                CategoryChipColors.chipUnselectedBg
+            }
+            CategoryChip(
+                text = category.name,
+                backgroundColor = chipBackgroundColor,
+                isSelected = isThisChipSelected,
+                onClick = {
+                    onCategoryClick(category.categoryId)
+                    onSave()
+                }
+            )
+
+        }
+    }
+}
+
