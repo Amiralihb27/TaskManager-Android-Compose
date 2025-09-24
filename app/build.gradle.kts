88522854275f6fd83ae9d1ccbe97ffac1a12f27e
Amirali <amiralihabibi82@gmail.com>
2025-09-24 11:09:08 +0330

    feat(app): Add initial AndroidManifest, build.gradle, and add icon
    This commit introduces the basic structure for the Android application:
    
    - **AndroidManifest.xml:** Defines the application, main activity, and intent filters.
    - **build.gradle.kts:** Configures the Android application plugin, dependencies (Compose, Room, Testing), build types, and compile options.
    - **ic_add.xml:** Adds a vector drawable for an "add" icon.

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.habibi.taskmanager"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    namespace = "com.habibi.taskmanager"
+}
+
+dependencies {
+    // Import the Compose BOM
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
+
+    // Testing
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+
+    // Mockito lets you create "fake" versions of your classes (like your DAO)
+    // We use mockito-kotlin for better Kotlin syntax.
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    // This is often needed to mock final classes, which Kotlin classes are by default.
+    testImplementation("org.mockito:mockito-inline:5.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation(kotlin("test"))
+
+    implementation("androidx.compose.material:material-icons-extended")
+
+}
